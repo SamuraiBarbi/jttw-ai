b6590cb6921010be2a62da43bce9a614c2176ea0
waywardspooky <waywardspooky@yahoo.com>
2023-12-19 13:19:41 -0500

    Create autogen_litellm_test.py

---
+++ tests/autogen_litellm_test.py
@@ -0,0 +1,30 @@
+from autogen import UserProxyAgent, ConversableAgent
+
+# Create our configuration for the LiteLLM endpoint. API Key is required but the value can be anything.
+config_list = [
+    {
+        "model": "ollama/openhermes2.5-mistral",
+        "base_url": "http://0.0.0.0:8000",
+        "api_key": "key-to-success"
+    }
+]
+
+# Create the agent that uses the LLM.
+assistant = ConversableAgent(
+    "agent", 
+    llm_config = {
+        "config_list": config_list
+    }
+)
+
+# Create the agent that represents the user in the conversation.
+user_proxy = UserProxyAgent(
+    "user", 
+    code_execution_config = False
+)
+
+# Let the assistant start the conversation.  It will end when the user types exit.
+assistant.initiate_chat(
+    user_proxy, 
+    message = "How can I help you today?"
+)
